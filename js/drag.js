725a3d27139fb5e738a8da541be334c95c5ce59f
Steven Colburn and William Kinzig <william@>
2018-06-18 14:34:41 -0700

    draw lines between two objects

--- js/drag.js
+++ js/drag.js
@@ -1,19 +1,31 @@
 //https://stackoverflow.com/questions/21864989/draw-lines-between-2-elements-in-html-page
 var newCanvas;
+var lineId = [];
 
 $(function()
 {
-  $( ".drag" ).draggable();
+  var clickIds = document.getElementsByClassName("draggable");
+
+  for (var i = 0; i < clickIds.length; i++)
+  {
+    clickIds[i].addEventListener('dblclick', getID,false);
+  }
+
   newCanvas = new Canvas(document.getElementById("canvas"));
   connectObjects();
 
+  $(".click-id").dblclick(function()
+  {
+    //console.log($("#0").offset());
 
+  });
 });
 
 function Canvas(canvasName)
 {
   this.canvas = canvasName;
   this.ctx = canvas.getContext("2d");
+  console.log(this.ctx);
 
   this.canvas.width = window.innerWidth;
   this.canvas.height = window.innerHeight;
@@ -24,14 +36,18 @@ function Canvas(canvasName)
 
 Canvas.prototype.push = function(id1,id2)
 {
-  this.connectors.push({from:id1,to:id2});
+  // this.connectors.push({from:id1,to:id2});
+  this.connectors.push([id1,id2]);
 }
 
 function connectObjects()
 {
-  newCanvas.push($("#0"),$("#0r"));
-  newCanvas.push($("#1"),$("#0r"));
-  newCanvas.push($("#2"),$("#2r"));
+  // newCanvas.push($("#0"),$("#0r"));
+  // newCanvas.push($("#1"),$("#0r"));
+  // newCanvas.push($("#2"),$("#2r"));
+  newCanvas.push("#0","#0r");
+  newCanvas.push("#1","#0r");
+  newCanvas.push("#2","#2r");
 
   connect();
 
@@ -49,18 +65,41 @@ function noop(){}
 
 function connect(){
   newCanvas.ctx.clearRect(0,0,newCanvas.canvas.width,newCanvas.canvas.height);
+  console.log(newCanvas.connectors[0]);
+  //newCanvas.ctx.translate(0,-60);
   for(var i=0;i<newCanvas.connectors.length;i++)
   {
-    var c=newCanvas.connectors[i];
+    // this.connectors.push({from:id1,to:id2});
+    var connection = newCanvas.connectors[i];
+
+    var c={from:$(connection[0]),to:$(connection[1])};
     var eFrom=c.from;
     var eTo=c.to;
     var pos1=eFrom.offset();
     var pos2=eTo.offset();
-    var size1=eFrom.size();
-    var size2=eTo.size();
     newCanvas.ctx.beginPath();
-    newCanvas.ctx.moveTo(pos1.left+eFrom.width()+3,pos1.top+eFrom.height()/2);
-    newCanvas.ctx.lineTo(pos2.left+3,pos2.top+eTo.height()/2);
+    //moveTo creates a point on cavas
+    newCanvas.ctx.moveTo(pos1.left,pos1.top+16);
+    //creates a line to the new point
+    newCanvas.ctx.lineTo(pos2.left,pos2.top+16);
+    //creates the line
     newCanvas.ctx.stroke();
+  }
+  //console.log(newCanvas.connectors[0])
+}
+
+function getID(event)
+{
+  lineId.push("#"+event.target.id)
+  console.log(event.target.id);
+  console.log(lineId);
+  if(lineId.length > 1)
+  {
+    if(lineId[0]!=lineId[1])
+    {
+      newCanvas.push(lineId[0],lineId[1])
+      connect();
+    }
+    lineId = [];
   }
 }

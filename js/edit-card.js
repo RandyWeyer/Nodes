55be6726f4185d8a41ad08f91a6be0b9dadf3b08
Hisato Kawaminami <hisatokawaminami@gmail.com>
2018-06-19 10:53:07 -0700

    add click to add new card

---
+++ js/edit-card.js
@@ -0,0 +1,37 @@
+$(function(){
+  $("#addCard").click(function(){
+    $("#2r").append('<div class="card" style="width: 18rem;">'+
+                    // '<form id="editForm">'+
+                    '<div class = form>'+
+                    '<div class="form-group">'+
+                    '<label for="cardTitle">Title</label>'+
+                    '<input type="text" class="form-control" id="cardTitle">'+
+                    '</div>'+
+                    '<div class="form-group">'+
+                    '<label for="cardNote">Note:</label>'+
+                    '<input type="text" class="form-control" id="cardNote">'+
+                    '</div>'+
+                    '<span class="btn btn-primary" id="saveCard">Save</span>'+
+                    '</div>'+
+                    // '</form>'+
+                    '<div class="card-body">'+
+                    '<h5 class="card-title">Card title</h5>'+
+                    '<p class="card-text">Some quick example text </p>'+
+                    '<span class="btn btn-primary" id="editCard">Edit</span>'+
+                    '</div>'+
+                    '</div> '
+                  );
+  })
+
+  $("#saveCard").click(function(){
+    var inputtedTitle = $("input#cardTitle").val();
+    var inputtedNote = $("input#cardNote").val();
+    $(".card-title").text(inputtedTitle);
+    $(".card-text").text(inputtedNote);
+    $(".form").hide();
+    $(".card-body").show();
+
+  })
+
+
+});

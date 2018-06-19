172b64cec0b44f09db1d7bb05eca059889e93253
Hisato Kawaminami and Matt Staudacher <hisato+matt@>
2018-06-19 15:55:21 -0700

    add textare and fix output for note

--- js/edit-card.js
+++ js/edit-card.js
@@ -1,6 +1,15 @@
+var globalId = 3;
+
 $(function(){
   $("#addCard").click(function(){
-    $("#2r").append('<div class="card" style="width: 18em;">'+
+    $(".insertDiv").append(
+
+      '<div id="'+globalId+'"class="draggable"><span class = "test"></span></div>'
+  );
+    globalId++;
+
+        $(".test").last().append(
+      '<div class="card" style="width: 18em;">'+
                     // '<form id="editForm">'+
                     '<div class = form>'+
                     '<div class="form-group">'+
@@ -9,7 +18,8 @@ $(function(){
                     '</div>'+
                     '<div class="form-group">'+
                     '<label for="cardNote">Note:</label>'+
-                    '<input type="text" class="form-control" id="cardNote">'+
+
+                    '<textarea type="text" lines="8" class="form-control" id="cardNote"></textarea>'+
                     '</div>'+
                     '<span class="btn btn-primary" id="saveCard">Save</span>'+
                     '</div>'+
@@ -20,12 +30,115 @@ $(function(){
                     '<span class="btn btn-primary" id="editCard">Edit</span>'+
                     '</div>'+
                     '</div>'
-                  );
 
+
+                );
+
+////////
+//https://stackoverflow.com/questions/21864989/draw-lines-between-2-elements-in-html-page
+var newCanvas;
+var lineId = [];
+
+$(function()
+{
+  var clickIds = document.getElementsByClassName("draggable");
+
+  for (var i = 0; i < clickIds.length; i++)
+  {
+    clickIds[i].addEventListener('dblclick', getID,false);
+    //clickIds[i].addEventListener('dblclick', getID,false);
+  }
+
+  newCanvas = new Canvas("canvas");
+  connectObjects();
+
+  $(".click-id").dblclick(function()
+  {
+    //console.log($("#0").offset());
+
+  });
+});
+
+function Canvas(canvasID)
+{
+  this.canvas = document.getElementById("canvas");
+  this.offset = $("#"+canvasID).offset();
+  this.ctx = canvas.getContext("2d");
+  this.canvas.width = window.innerWidth;
+  this.canvas.height = window.innerHeight;
+  this.ctx.lineWidth = 3;
+  this.connectors = [];
+}
+
+Canvas.prototype.push = function(id1,id2)
+{
+  this.connectors.push([id1,id2]);
+}
+
+function connectObjects()
+{
+  connect();
+  $(".draggable").draggable({
+    // event handlers
+    start: noop,
+    drag:  connect,
+    stop:  noop
+  });
+}
+
+function noop(){}
+
+function connect(){
+  newCanvas.ctx.clearRect(0,0,newCanvas.canvas.width,newCanvas.canvas.height);
+  //console.log(newCanvas.connectors[0]);
+  console.log(newCanvas.offset.top);
+  var topOffset = newCanvas.offset.top;
+  var leftOffset = newCanvas.offset.left;
+  //newCanvas.ctx.translate(0,-60);
+  for(var i=0;i<newCanvas.connectors.length;i++)
+  {
+    // this.connectors.push({from:id1,to:id2});
+    var connection = newCanvas.connectors[i];
+
+    var c={from:$(connection[0]),to:$(connection[1])};
+    var eFrom=c.from;
+    var eTo=c.to;
+    var pos1=eFrom.offset();
+    var pos2=eTo.offset();
+    newCanvas.ctx.beginPath();
+    //moveTo creates a point on cavas
+    newCanvas.ctx.moveTo(pos1.left+16-leftOffset,pos1.top+16-topOffset);
+    //creates a line to the new point
+    newCanvas.ctx.lineTo(pos2.left+16-leftOffset,pos2.top+16-topOffset);
+    //console.log(newCanvas.connectors[0])
+    //creates the line
+    newCanvas.ctx.stroke();
+  }
+  //console.log(newCanvas.connectors[0])
+}
+
+function getID(event)
+{
+  lineId.push("#"+event.target.id)
+  console.log(event.target.id);
+  console.log(lineId);
+  if(lineId.length > 1)
+  {
+    if(lineId[0]!=lineId[1])
+    {
+      newCanvas.push(lineId[0],lineId[1])
+      connect();
+    }
+    lineId = [];
+  }
+}
+
+
+////////////////////
     $("#saveCard").last().click(function(){
 
       var inputtedTitle = $("input#cardTitle").val();
-      var inputtedNote = $("input#cardNote").val();
+      var inputtedNote = $("textarea#cardNote").val();
       $(".card-title").text(inputtedTitle);
       $(".card-text").text(inputtedNote);
       $(".form").hide();
--- js/scripts.js
+++ js/scripts.js
@@ -1,97 +1,97 @@
-/*
-$(document).ready(function(){
-  var canvas = document.getElementById('canvas');
-
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-  var raf;
-  var running = false;
-  var dragging = false;
-
-  var ball = {
-    x: 100,
-    y: 100,
-    vx: 5,
-    vy: 1,
-    radius: 25,
-    color: 'blue',
-    draw: function() {
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-      ctx.closePath();
-      ctx.fillStyle = this.color;
-      ctx.fill();
-    }
-  };
-
-  function grid() {
-    var w = canvas.width,
-        h = canvas.height;
-
-    /**
-     * i is used for both x and y to draw
-     * a line every 5 pixels starting at
-     * .5 to offset the canvas edges
-     */
-    for(var i = .5; i < w || i < h; i += 15) {
-        // draw horizontal lines
-        ctx.moveTo( i, 0 );
-        ctx.lineTo( i, h);
-        // draw vertical lines
-        ctx.moveTo( 0, i );
-        ctx.lineTo( w, i);
-    }
-    ctx.strokeStyle = 'hsla(0, 0%, 40%, .5)';
-    ctx.stroke();
-}
-
-grid();
-
-  function clear() {
-    ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-    grid();
-  }
-
-  function draw() {
-    clear();
-    ball.draw();
-
-    raf = window.requestAnimationFrame(draw);
-  }
-
-  canvas.addEventListener('mousedown', function(e) {
-    if (!dragging) {
-      raf = window.requestAnimationFrame(draw);
-      dragging = true;
-    }
-  });
-
-  canvas.addEventListener('mousemove', function(e) {
-    if (dragging) {
-      clear();
-      ball.x = e.clientX-canvas.getBoundingClientRect().left;
-      ball.y = e.clientY-canvas.getBoundingClientRect().top;
-      ball.draw();
-    }
-  });
-
-  canvas.addEventListener('mouseup', function(e) {
-    if (dragging) {
-      raf = window.requestAnimationFrame(draw);
-      dragging = false;
-    }
-  });
-
-  ball.draw();
-
-
-
-
-
-});
-
-*/
+// /*
+// $(document).ready(function(){
+//   var canvas = document.getElementById('canvas');
+//
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+//
+//   var canvas = document.getElementById('canvas');
+//   var ctx = canvas.getContext('2d');
+//   var raf;
+//   var running = false;
+//   var dragging = false;
+//
+//   var ball = {
+//     x: 100,
+//     y: 100,
+//     vx: 5,
+//     vy: 1,
+//     radius: 25,
+//     color: 'blue',
+//     draw: function() {
+//       ctx.beginPath();
+//       ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+//       ctx.closePath();
+//       ctx.fillStyle = this.color;
+//       ctx.fill();
+//     }
+//   };
+//
+//   function grid() {
+//     var w = canvas.width,
+//         h = canvas.height;
+//
+//     /**
+//      * i is used for both x and y to draw
+//      * a line every 5 pixels starting at
+//      * .5 to offset the canvas edges
+//      */
+//     for(var i = .5; i < w || i < h; i += 15) {
+//         // draw horizontal lines
+//         ctx.moveTo( i, 0 );
+//         ctx.lineTo( i, h);
+//         // draw vertical lines
+//         ctx.moveTo( 0, i );
+//         ctx.lineTo( w, i);
+//     }
+//     ctx.strokeStyle = 'hsla(0, 0%, 40%, .5)';
+//     ctx.stroke();
+// }
+//
+// grid();
+//
+//   function clear() {
+//     ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+//     ctx.fillRect(0,0,canvas.width,canvas.height);
+//     grid();
+//   }
+//
+//   function draw() {
+//     clear();
+//     ball.draw();
+//
+//     raf = window.requestAnimationFrame(draw);
+//   }
+//
+//   canvas.addEventListener('mousedown', function(e) {
+//     if (!dragging) {
+//       raf = window.requestAnimationFrame(draw);
+//       dragging = true;
+//     }
+//   });
+//
+//   canvas.addEventListener('mousemove', function(e) {
+//     if (dragging) {
+//       clear();
+//       ball.x = e.clientX-canvas.getBoundingClientRect().left;
+//       ball.y = e.clientY-canvas.getBoundingClientRect().top;
+//       ball.draw();
+//     }
+//   });
+//
+//   canvas.addEventListener('mouseup', function(e) {
+//     if (dragging) {
+//       raf = window.requestAnimationFrame(draw);
+//       dragging = false;
+//     }
+//   });
+//
+//   ball.draw();
+//
+//
+//
+//
+//
+// });
+//
+// */

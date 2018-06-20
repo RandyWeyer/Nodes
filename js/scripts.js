57996a771a2c2555c0d87ce4a90b14ac9aa10182
Steven Colburn <steven@>
2018-06-20 14:26:13 -0700

    changed connect into a method

--- js/scripts.js
+++ js/scripts.js
@@ -28,8 +28,7 @@ const cardHtml = (
   '</div>'
 )
 
-$(function()
-{
+$(function(){
 
   //Start Drag
   $(".draggable").dblclick(function()
@@ -45,191 +44,186 @@ $(function()
     //append it to input-card
     $("#input-card").append(element);
     //make it "draggable"
-    element.draggable({
-      // event handlers
-      start: noop,
-      drag:  connect,
-      stop:  noop
-    }).dblclick(function(){addId(this);});
+    element.draggable(
+      {
+        // event handlers
+        start: noop(),
+        drag:  function(){
+          newCanvas.connect();
+        },
+        stop:  noop()
+      }).dblclick(function(){addId(this);});
 
-    //Add Click events to buttons save-card and edit-card in the card using DOM Traversal
-    element.find(".save-card").click(function(){saveInfo(this);});
-    element.find(".edit-card").click(function(){editInfo(this);});
-  });
-  newCanvas = new Canvas("canvas");
-
-  $("#btn-color").click(function()
-  {
-    //getHexColor();
-  });
-
-});
-
-function getHexColor(){
-  return $("#color-selection").val();
-}
-
-function resizeCanvas()
-{
-  newCanvas.setToWindow();
-  connect();
-}
-
-function saveInfo(id)
-{
-  var parent = $(id).parent().parent();
-  var title = parent.find("#input-title").val();
-  var note = parent.find("#input-note").val();
-  parent.find("#output-title").text(title);
-  parent.find("#output-note").text(note);
-  parent.find(".card-body").show();
-  parent.find(".input-form").hide();
-}
-
-function editInfo(id)
-{
-  var parent = $(id).parent().parent();
-  var title = parent.find("#output-title").text();
-  var note = parent.find("#output-note").text();
-  parent.find("#input-title").val(title);
-  parent.find("#input-note").val(note);
-  parent.find(".card-body").hide();
-  parent.find(".input-form").show();
-}
-
-function noop(){}
-
-function connect(){
-  newCanvas.ctx.clearRect(0,0,newCanvas.canvas.width,newCanvas.canvas.height);
-  //console.log(newCanvas.connectors[0]);
-  //console.log(newCanvas.offset.top);
-  var topOffset = newCanvas.offset.top;
-  var leftOffset = newCanvas.offset.left;
-  //newCanvas.ctx.translate(0,-60);
-  for(var i=0;i<newCanvas.connectors.length;i++)
-  {
-    // this.connectors.push({from:id1,to:id2});
-    var connection = newCanvas.connectors[i];
-
-    var c={from:$(connection[0]),to:$(connection[1])};
-    var eFrom=c.from;
-    var eTo=c.to;
-    var pos1=eFrom.offset();
-    var pos2=eTo.offset();
-    newCanvas.ctx.beginPath();
-    //moveTo creates a point on cavas
-    newCanvas.ctx.moveTo(pos1.left+16-leftOffset,pos1.top+16-topOffset);
-    //creates a line to the new point
-    newCanvas.ctx.lineTo(pos2.left+16-leftOffset,pos2.top+16-topOffset);
-    //console.log(newCanvas.connectors[0])
-    //creates the line
-    newCanvas.ctx.stroke();
-  }
-  //console.log(newCanvas.connectors[0])
-}
-
-function addId(tempId)
-{
-  lineId.push(tempId);
-
-  if($(tempId).hasClass("card-selected"))
-  {
-    $(tempId).removeClass("card-selected");
-  }
-  else
-  {
-    $(tempId).addClass("card-selected");
-  }
-
-  if(lineId.length > 1)
-  {
-    //console.log(lineId);
-    var reverseArr = lineId.slice();
-    reverseArr.reverse();
-    if(newCanvas.indexOf(lineId)!=-1)
-    {
-      console.log("Test One");
-      newCanvas.removeAt(newCanvas.indexOf(lineId));
-    }
-    else if(newCanvas.indexOf(reverseArr)!=-1)
-    {
-      console.log("Test Two");
-      newCanvas.removeAt(newCanvas.indexOf(reverseArr));
-    }
-    else if(lineId[0]!=lineId[1])
-    {
-      console.log("Test Three");
-      newCanvas.push(lineId[0],lineId[1]);
-
-    }
-    lineId.forEach(function(line){
-      $(line).removeClass("card-selected");
+      //Add Click events to buttons save-card and edit-card in the card using DOM Traversal
+      element.find(".save-card").click(function(){saveInfo(this);});
+      element.find(".edit-card").click(function(){editInfo(this);});
     });
 
-    lineId = [];
-    connect();
-  }
-}
+    newCanvas = new Canvas("canvas");
 
-function Canvas(canvasID)
-{
-  this.canvas = document.getElementById("canvas");
-  this.offset = $("#"+canvasID).offset();
-  this.ctx = canvas.getContext("2d");
-  this.setToWindow();
-  this.ctx.lineWidth = 3;
-  this.connectors = [];
-}
-
-Canvas.prototype.setWidth = function(input)
-{
-  this.canvas.width = input;
-  this.width = input;
-}
-
-Canvas.prototype.setHeight = function(input)
-{
-  this.canvas.height = input;
-  this.height = input;
-}
-
-Canvas.prototype.setToWindow = function()
-{
-  this.setWidth(window.innerWidth);
-  this.setHeight(window.innerHeight);
-}
-
-Canvas.prototype.push = function(id1,id2)
-{
-  this.connectors.push([id1,id2]);
-}
-
-Canvas.prototype.indexOf = function(idArr)
-{
-  var counter = 0;
-  var location = -1;
-
-  this.connectors.forEach(function(connector){
-    if(connector[0]===idArr[0]&&connector[1]===idArr[1])
+    $("#btn-color").click(function()
     {
-      location = counter;
-    }
-    counter++;
-  });
-  return (location);
-}
+      //getHexColor();
+    });
 
-Canvas.prototype.removeAt = function(location)
-{
-  if(this.connectors.length === 1)
+  });
+
+  function getHexColor(){
+    return $("#color-selection").val();
+  }
+
+  function resizeCanvas()
   {
-    this.connectors.pop();
+    newCanvas.setToWindow();
+    newCanvas.connect();
   }
-  else if (location === 0) {
-    this.connectors.shift();
-  }
-  else
+
+  function saveInfo(id)
   {
-    this.connectors.splice(location,location);
+    var parent = $(id).parent().parent();
+    var title = parent.find("#input-title").val();
+    var note = parent.find("#input-note").val();
+    parent.find("#output-title").text(title);
+    parent.find("#output-note").text(note);
+    parent.find(".card-body").show();
+    parent.find(".input-form").hide();
   }
-}
+
+  function editInfo(id)
+  {
+    var parent = $(id).parent().parent();
+    var title = parent.find("#output-title").text();
+    var note = parent.find("#output-note").text();
+    parent.find("#input-title").val(title);
+    parent.find("#input-note").val(note);
+    parent.find(".card-body").hide();
+    parent.find(".input-form").show();
+  }
+
+  function noop(){}
+
+  Canvas.prototype.connect = function ()
+  {
+    this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
+    for(var i=0;i<this.connectors.length;i++)
+    {
+      var connection = this.connectors[i];
+
+      var c={from:$(connection[0]),to:$(connection[1])};
+      var pos1=c.from.offset();
+      var pos2=c.to.offset();
+      var positionOne = [pos1.left+16-this.offset.left,pos1.top+16-this.offset.top];
+      var positionTwo = [pos2.left+16-this.offset.left,pos2.top+16-this.offset.top];
+      this.ctx.beginPath();
+      //moveTo creates a point on cavas
+      this.ctx.moveTo(positionOne[0],positionOne[1]);
+      //creates a line to the new point
+      this.ctx.lineTo(positionTwo[0],positionTwo[1]);
+      //console.log(this.connectors[0])
+      //creates the line
+      this.ctx.stroke();
+    }
+
+  }
+
+  function addId(tempId)
+  {
+    lineId.push(tempId);
+
+    if($(tempId).hasClass("card-selected"))
+    {
+      $(tempId).removeClass("card-selected");
+    }
+    else
+    {
+      $(tempId).addClass("card-selected");
+    }
+
+    if(lineId.length > 1)
+    {
+      //console.log(lineId);
+      var reverseArr = lineId.slice();
+      reverseArr.reverse();
+      if(newCanvas.indexOf(lineId)!=-1)
+      {
+        newCanvas.removeAt(newCanvas.indexOf(lineId));
+      }
+      else if(newCanvas.indexOf(reverseArr)!=-1)
+      {
+        newCanvas.removeAt(newCanvas.indexOf(reverseArr));
+      }
+      else if(lineId[0]!=lineId[1])
+      {
+        newCanvas.push(lineId[0],lineId[1]);
+      }
+      lineId.forEach(function(line){
+        $(line).removeClass("card-selected");
+      });
+
+      lineId = [];
+      newCanvas.connect();
+    }
+  }
+
+  function Canvas(canvasID)
+  {
+    this.canvas = document.getElementById("canvas");
+    this.offset = $("#"+canvasID).offset();
+    this.ctx = canvas.getContext("2d");
+    this.setToWindow();
+    this.ctx.lineWidth = 3;
+    this.connectors = [];
+  }
+
+  Canvas.prototype.setWidth = function(input)
+  {
+    this.canvas.width = input;
+    this.width = input;
+  }
+
+  Canvas.prototype.setHeight = function(input)
+  {
+    this.canvas.height = input;
+    this.height = input;
+  }
+
+  Canvas.prototype.setToWindow = function()
+  {
+    this.setWidth(window.innerWidth);
+    this.setHeight(window.innerHeight);
+  }
+
+  Canvas.prototype.push = function(id1,id2)
+  {
+    this.connectors.push([id1,id2]);
+  }
+
+  Canvas.prototype.indexOf = function(idArr)
+  {
+    var counter = 0;
+    var location = -1;
+
+    this.connectors.forEach(function(connector){
+      if(connector[0]===idArr[0]&&connector[1]===idArr[1])
+      {
+        location = counter;
+      }
+      counter++;
+    });
+    return (location);
+  }
+
+  Canvas.prototype.removeAt = function(location)
+  {
+    if(this.connectors.length === 1)
+    {
+      this.connectors.pop();
+    }
+    else if (location === 0) {
+      this.connectors.shift();
+    }
+    else
+    {
+      this.connectors.splice(location,location);
+    }
+  }
